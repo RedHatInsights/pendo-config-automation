7f09aacd2ef91da3d7c87fc300297670b154bf7d
Ian Page Hands <iphands@gmail.com>
2020-10-27 12:38:17 -0400

    Large refactor
    - //cloud.redhat.com now added automatically
    - features object now supports _scope
    _scope sets a selector that get prepended to all selectors in the features obj

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import client as client
 import time
 
 DELAY=5
+DRY_RUN=False
 
 def get_skip_list():
     with open('./secrets/skiplist.yml') as data:
@@ -21,16 +22,34 @@ with open('./data/config.yml', 'r') as data:
             continue
 
         print('Creating group: [{}]'.format(name))
-        pendo_group = client.create_group_idempotent(name, client.get_color(name, group))
+        if not DRY_RUN:
+            pendo_group = client.create_group_idempotent(name, client.get_color(name, group))
 
         if 'pages' in group:
             for page_name, page in group['pages'].items():
+                for i, url in enumerate(page['url_rules']):
+                    page['url_rules'][i] = '//cloud.redhat.com{}'.format(page['url_rules'][i])
+
                 print('- Creating page: [{}] {}'.format(page_name, page))
-                client.create_page_in_group(pendo_group, page_name, page)
-                time.sleep(DELAY)
+                if not DRY_RUN:
+                    client.create_page_in_group(pendo_group, page_name, page)
+                    time.sleep(DELAY)
 
         if 'features' in group:
+            scope = False
+            if '_scope' in group['features']:
+                scope = group['features']['_scope']
+                del group['features']['_scope']
+
             for feature_name, feature in group['features'].items():
+                if scope:
+                    for i, selector in enumerate(feature['selectors']):
+                        if selector.startswith('['):
+                            feature['selectors'][i] = '{}{}'.format(scope, feature['selectors'][i])
+                        else:
+                            feature['selectors'][i] = '{} {}'.format(scope, feature['selectors'][i])
+
                 print('- Creating feature: [{}] {}'.format(feature_name, feature))
-                client.create_feature_in_group(pendo_group, feature_name, feature)
-                time.sleep(DELAY)
+                if not DRY_RUN:
+                    client.create_feature_in_group(pendo_group, feature_name, feature)
+                    time.sleep(DELAY)
